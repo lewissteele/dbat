d54daa284d829d41d9ea771634a7662bcc26e027
Lewis Steele <lewis.steele@posteo.net>
2024-08-22 14:42:26 +0100

    Run prettier

--- commands/connect.js
+++ commands/connect.js
@@ -4,7 +4,7 @@ import { Args, Command } from "@oclif/core";
 import readline from "node:readline/promises";
 import { stdin, stdout } from "node:process";
 import { Sequelize } from "sequelize";
-import Table from 'cli-table3';
+import Table from "cli-table3";
 
 export default class Connect extends Command {
   static args = {
@@ -38,22 +38,22 @@ export default class Connect extends Command {
     rl.on("line", async (line) => {
       const [results, meta] = await sequelize.query(line, { raw: true });
 
-      if (!results.length){
+      if (!results.length) {
         rl.prompt();
-        return
+        return;
       }
 
-      const headers = Object.keys(results[0])
-      const rows = results.map(row => Object.values(row))
+      const headers = Object.keys(results[0]);
+      const rows = results.map((row) => Object.values(row));
 
       const table = new Table({
         head: headers,
         rows: rows,
-      })
+      });
 
-      table.push(...rows)
+      table.push(...rows);
 
-      this.log(table.toString())
+      this.log(table.toString());
 
       rl.prompt();
     });
