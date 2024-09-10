05938fbde1fdfb8700a1afac83893d78f8df24ce
Lewis Steele <lewis.steele@posteo.net>
2024-09-10 16:02:35 +0100

    Move config into module

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,9 +1,10 @@
-const BaseCommand = require("../base-command");
+const { getConfig } = require("../api/config");
+const { Command } = require("@oclif/core");
 
-module.exports = class List extends BaseCommand {
+module.exports = class List extends Command {
   static description = "show databases";
   async run() {
-    const config = await this.getConfig();
+    const config = await getConfig();
     const databases = Object.keys(config.databases);
 
     if (!databases.length) {
