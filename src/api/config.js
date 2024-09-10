29c55b6154c741be43e95ff2b3ef649aecc03a53
Lewis Steele <lewis.steele@posteo.net>
2024-09-10 16:10:09 +0100

    Run prettier

--- src/api/config.js
+++ src/api/config.js
@@ -1,23 +1,22 @@
 const fs = require("fs-extra");
 const path = require("path");
 
-module.exports.getConfig = async function() {
+module.exports.getConfig = async function () {
   const path = getPath();
 
-  if (await fs.exists(path) {
+  if (await fs.exists(path)) {
     return await fs.readJson(path, {
       throws: false,
     });
   }
 
   return { databases: {} };
-}
+};
 
-module.exports.setConfig = async function(config) {
+module.exports.setConfig = async function (config) {
   await fs.writeJson(getPath(), config);
-}
+};
 
 function getPath() {
   return path.join(global.config.configDir, "config.json");
 }
-
