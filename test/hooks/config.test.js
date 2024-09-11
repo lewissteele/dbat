83e8cb464dfa15f683c8429cb56f8e1e6cd81106
Lewis Steele <lewis.steele@posteo.net>
2024-09-11 14:04:15 +0100

    Install faker

--- test/hooks/config.test.js
+++ test/hooks/config.test.js
@@ -1,11 +1,12 @@
 const assert = require("node:assert");
-const test = require("node:test");
 const configHook = require("../../src/hooks/config");
+const test = require("node:test");
+const { faker } = require("@faker-js/faker");
 
 test("config hook", async () => {
   const options = {
     config: {
-      configDir: "/home/lewis/.config/dbat",
+      configDir: faker.system.directoryPath(),
     },
   };
 
