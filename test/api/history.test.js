5d18215cb75335089aec7a52acfc860735056580
Lewis Steele <lewis.steele@posteo.net>
2024-09-14 16:22:23 +0100

    Fix test

--- test/api/history.test.js
+++ test/api/history.test.js
@@ -3,20 +3,22 @@ const path = require("path");
 const { faker } = require("@faker-js/faker");
 const { getHistory, pushToHistory } = require("../../src/api/history");
 
+beforeAll(() => {
+  const fakePath = faker.system.directoryPath();
+
+  global.config = { configDir: fakePath };
+
+  mock({ [path.join(fakePath, "history.json")]: "" });
+});
+
 test("history saves to file", async () => {
-  global.config = {
-    configDir: faker.system.directoryPath(),
-  };
-
-  const configFile = path.join(global.config.configDir, "history.json");
-
-  mock({
-    [configFile]: "",
-  });
-
   const query = "select * from test";
 
   await pushToHistory(query);
 
-  expect((await getHistory()).includes(query)).toBeTruthy()
+  expect((await getHistory()).includes(query)).toBe(true);
+});
+
+afterAll(() => {
+  mock.restore();
 });
