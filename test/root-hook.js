dd03814d69ad1ea703d38944d1daa20ddb846e64
Lewis Steele <lewis.steele@posteo.net>
2024-09-16 22:16:54 +0100

    Use named functions

--- test/root-hook.js
+++ test/root-hook.js
@@ -1,6 +1,10 @@
 const fs = require("fs-extra");
 
 exports.mochaHooks = {
-  beforeAll: () => (global.config = { configDir: "config" }),
-  afterAll: () => fs.remove("config"),
+  beforeAll() {
+    global.config = { configDir: "config" };
+  },
+  afterAll() {
+    fs.remove("config");
+  },
 };
