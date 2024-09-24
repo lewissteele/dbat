649cf447dba556184b91b0e424e106f2994f44f3
Lewis Steele <lewis.steele@posteo.net>
2024-09-24 15:25:10 +0100

    Fix tests

--- test/commands/list.test.js
+++ test/commands/list.test.js
@@ -1,22 +1,19 @@
 const List = require("../../src/commands/list");
 const assert = require("node:assert");
-const fs = require("fs-extra");
 const { captureOutput } = require("@oclif/test");
 const { saveDatabase, removeDatabase } = require("../../src/api/database");
 
-it("lists all saved database connections", async () => {
-  const expected = "test";
+beforeEach(() => removeDatabase("test"));
 
-  saveDatabase(expected, {
+it("lists all saved database connections", async () => {
+  await saveDatabase("test", {
     dialect: "sqlite",
     storage: ":memory:",
   });
 
   const { stdout } = await captureOutput(() => List.run());
 
-  assert.equal(stdout.trim(), expected);
-
-  removeDatabase(removeDatabase);
+  assert.equal(stdout.trim(), "test");
 });
 
 it("still works when there are no databases", async () => {
