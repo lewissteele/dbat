6810a40b827c03a0d67cfaee665723ede265c95c
Lewis Steele <lewis.steele@posteo.net>
2024-10-20 15:08:14 +0100

    Add support for postgres

--- internal/db/user.go
+++ internal/db/user.go
@@ -3,8 +3,10 @@ package db
 import (
 	"strings"
 
+	"github.com/gookit/goutil/dump"
 	"github.com/lewissteele/dbat/internal/model"
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -18,19 +20,28 @@ const (
 )
 
 func UserDB(name string) *gorm.DB {
-	userDB, err := gorm.Open(mysql.Open(
-		dsn(name)),
-		&gorm.Config{
-			//Logger: logger.Default.LogMode(logger.Silent),
-			SkipDefaultTransaction: true,
-		},
-	)
+	var userDB model.Database
+	LocalDB.Where("name = ?", name).Find(&userDB)
+
+	var dialector gorm.Dialector
+
+	switch Driver(userDB.Driver) {
+	case PostgreSQL:
+		dialector = postgres.Open(dsn(&userDB))
+	default:
+		dialector = mysql.Open(dsn(&userDB))
+	}
+
+	gormDB, err := gorm.Open(dialector, &gorm.Config{
+		//Logger: logger.Default.LogMode(logger.Silent),
+		SkipDefaultTransaction: true,
+	})
 
 	if err != nil {
 		panic("could not connect")
 	}
 
-	return userDB
+	return gormDB
 }
 
 func UserDBNames() []string {
@@ -53,10 +64,22 @@ func Port(driver Driver) string {
 	return "3306"
 }
 
-func dsn(name string) string {
-	db := model.Database{}
-
-	LocalDB.Where("name = ?", name).First(&db)
+func dsn(db *model.Database) string {
+	if Driver(db.Driver) == PostgreSQL {
+		return strings.Join([]string{
+			"host=",
+			db.Host,
+			" ",
+			"user=",
+			db.User,
+			" ",
+			"password=",
+			db.Pass,
+			" ",
+			"port=",
+			db.Port,
+		}, "")
+	}
 
 	return strings.Join([]string{
 		db.User,
