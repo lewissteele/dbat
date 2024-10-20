99ed2749ffda6ec831422bbba368c6fad377fa19
Lewis Steele <lewis.steele@posteo.net>
2024-10-20 16:43:24 +0100

    Run go fmt

--- internal/input/input.go
+++ internal/input/input.go
@@ -7,26 +7,26 @@ import (
 )
 
 var keywords = []string{
-  "from",
-  "select",
-  "update",
-  "where",
+	"from",
+	"select",
+	"update",
+	"where",
 }
 
 func Completer(d prompt.Document) []prompt.Suggest {
-  s := []prompt.Suggest{}
+	s := []prompt.Suggest{}
 
-  if len(strings.Trim(d.Text, "")) == 0 {
-    return s
-  }
+	if len(strings.Trim(d.Text, "")) == 0 {
+		return s
+	}
 
-  for _, keyword := range keywords {
-    if strings.Contains(keyword, d.Text) {
-      s = append(s, prompt.Suggest{
-        Text: keyword,
-      })
-    }
-  }
+	for _, keyword := range keywords {
+		if strings.Contains(keyword, d.Text) {
+			s = append(s, prompt.Suggest{
+				Text: keyword,
+			})
+		}
+	}
 
-  return s
+	return s
 }
