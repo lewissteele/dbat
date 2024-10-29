b54617a8b22e13324c01bae1ac9fe834bba59577
Lewis Steele <lewis.steele@posteo.net>
2024-10-29 15:55:15 +0000

    Sort completion by string similarity

--- internal/input/completer.go
+++ internal/input/completer.go
@@ -1,12 +1,16 @@
 package input
 
 import (
+	"sort"
 	"strings"
 
+	"github.com/adrg/strutil"
+	"github.com/adrg/strutil/metrics"
 	"github.com/c-bata/go-prompt"
 )
 
 func Completer(d prompt.Document) []prompt.Suggest {
+	m := make(map[float64]string)
 	s := []prompt.Suggest{}
 
 	if len(strings.TrimSpace(d.Text)) == 0 {
@@ -18,10 +22,26 @@ func Completer(d prompt.Document) []prompt.Suggest {
 
 	for _, keyword := range keywords {
 		if strings.Contains(keyword, currentWord) {
-			s = append(s, prompt.Suggest{
-				Text: keyword,
-			})
+			similarity := strutil.Similarity(
+				currentWord,
+				keyword,
+				metrics.NewLevenshtein(),
+			)
+
+			m[similarity] = keyword
 		}
+	}
+
+	keys := make([]float64, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Float64s(keys)
+
+	for _, k := range keys {
+		s = append(s, prompt.Suggest{
+			Text: m[k],
+		})
 	}
 
 	return s
