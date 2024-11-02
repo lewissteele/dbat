ffb58620c3db85887c125c6c83b33fe883f34075
Lewis Steele <lewis.steele@posteo.net>
2024-11-02 15:11:42 +0000

    Add conn method to db model

--- internal/model/database.go
+++ internal/model/database.go
@@ -1,6 +1,12 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"strings"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
 
 type Database struct {
 	gorm.Model
@@ -10,4 +16,32 @@ type Database struct {
 	Pass   string
 	Port   string
 	User   string
+}
+
+func (d Database) Conn() *gorm.DB {
+	conn, err := gorm.Open(
+		mysql.Open(d.dsn()),
+		&gorm.Config{
+			Logger:                 logger.Default.LogMode(logger.Silent),
+			SkipDefaultTransaction: true,
+		})
+
+	if err != nil {
+		panic("could not connect")
+	}
+
+	return conn
+}
+
+func (d Database) dsn() string {
+	return strings.Join([]string{
+		d.User,
+		":",
+		d.Pass,
+		"@tcp(",
+		d.Host,
+		":",
+		d.Port,
+		")/?parseTime=true",
+	}, "")
 }
