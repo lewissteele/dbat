5217cd8418949f8803559b5295f3cd45fa862820
Lewis Steele <lewis.steele@posteo.net>
2024-11-13 19:37:49 +0000

    Move db connection management back into db package

--- internal/db/user.go
+++ internal/db/user.go
@@ -1,11 +1,44 @@
 package db
 
-import "github.com/lewissteele/dbat/internal/model"
+import (
+	"strings"
 
-func UserDB(name string) *model.Database {
-	var userDB model.Database
-	LocalDB.Where("name = ?", name).Find(&userDB)
-	return &userDB
+	"github.com/lewissteele/dbat/internal/model"
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var UserDB model.Database
+var Conn *gorm.DB
+
+func Connect(name string) (*gorm.DB, *model.Database) {
+	LocalDB.Where("name = ?", name).Find(&UserDB)
+
+	var dialector gorm.Dialector
+
+	switch Driver(UserDB.Driver) {
+	case PostgreSQL:
+		dialector = postgres.Open(dsn(UserDB))
+	default:
+		dialector = mysql.Open(dsn(UserDB))
+	}
+
+	var err error
+
+	Conn, err = gorm.Open(
+		dialector,
+		&gorm.Config{
+			Logger:                 logger.Default.LogMode(logger.Silent),
+			SkipDefaultTransaction: true,
+		})
+
+	if err != nil {
+		panic("could not connect")
+	}
+
+	return Conn, &UserDB
 }
 
 func UserDBNames() []string {
@@ -21,9 +54,38 @@ func UserDBNames() []string {
 	return names
 }
 
-func Port(driver model.Driver) string {
-	if driver == model.PostgreSQL {
+func Port(d Driver) string {
+	if d == PostgreSQL {
 		return "5432"
 	}
 	return "3306"
+}
+
+func dsn(d model.Database) string {
+	if Driver(d.Driver) == PostgreSQL {
+		return strings.Join([]string{
+			"host=",
+			d.Host,
+			" ",
+			"user=",
+			d.User,
+			" ",
+			"password=",
+			d.Pass,
+			" ",
+			"port=",
+			d.Port,
+		}, "")
+	}
+
+	return strings.Join([]string{
+		d.User,
+		":",
+		d.Pass,
+		"@tcp(",
+		d.Host,
+		":",
+		d.Port,
+		")/?parseTime=true",
+	}, "")
 }
