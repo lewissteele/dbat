a2ece51ba955582e0c6e0e23b985176600ea3433
Lewis Steele <lewis.steele@posteo.net>
2024-11-14 20:07:21 +0000

    Use huh to get driver

--- cmd/add.go
+++ cmd/add.go
@@ -3,9 +3,9 @@ package cmd
 import (
 	"errors"
 
+	"github.com/charmbracelet/huh"
 	"github.com/lewissteele/dbat/internal/db"
 	"github.com/lewissteele/dbat/internal/model"
-	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
@@ -18,59 +18,85 @@ func init() {
 }
 
 func run(cmd *cobra.Command, args []string) {
-	selectDriver := promptui.Select{
-		Items: []db.Driver{
-			db.MariaDB,
-			db.MySQL,
-			db.PostgreSQL,
-			db.SQLite,
-		},
-		Label:    "driver",
-		HideHelp: true,
+	var (
+		driver string
+		host string
+		name string
+		pass string
+		port string
+		user string
+	)
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().Title("driver").Options(
+				huh.NewOption(string(db.MariaDB), string(db.MariaDB)),
+				huh.NewOption(string(db.MySQL), string(db.MySQL)),
+				huh.NewOption(string(db.PostgreSQL), string(db.PostgreSQL)),
+				huh.NewOption(string(db.SQLite), string(db.SQLite)),
+			),
+		),
+	)
+
+	err := form.Run()
+
+	if err != nil {
+		panic(err)
 	}
 
-	_, driver, _ := selectDriver.Run()
+	// selectDriver := promptui.Select{
+	// 	Items: []db.Driver{
+	// 		db.MariaDB,
+	// 		db.MySQL,
+	// 		db.PostgreSQL,
+	// 		db.SQLite,
+	// 	},
+	// 	Label:    "driver",
+	// 	HideHelp: true,
+	// }
+	//
+	// _, driver, _ := selectDriver.Run()
+	//
+	// prompts := []promptui.Prompt{
+	// 	promptui.Prompt{
+	// 		Label:    "host",
+	// 		Validate: isNotBlank,
+	// 	},
+	// 	promptui.Prompt{
+	// 		Label:    "username",
+	// 		Validate: isNotBlank,
+	// 	},
+	// 	promptui.Prompt{
+	// 		Label: "password",
+	// 		Mask:  '*',
+	// 	},
+	// 	promptui.Prompt{
+	// 		AllowEdit: true,
+	// 		Default:   db.Port(db.Driver(driver)),
+	// 		Label:     "port",
+	// 		Validate:  isNotBlank,
+	// 	},
+	// }
 
-	prompts := []promptui.Prompt{
-		promptui.Prompt{
-			Label:    "host",
-			Validate: isNotBlank,
-		},
-		promptui.Prompt{
-			Label:    "username",
-			Validate: isNotBlank,
-		},
-		promptui.Prompt{
-			Label: "password",
-			Mask:  '*',
-		},
-		promptui.Prompt{
-			AllowEdit: true,
-			Default:   db.Port(db.Driver(driver)),
-			Label:     "port",
-			Validate:  isNotBlank,
-		},
-	}
+	// var results []string
 
-	var results []string
+	// for _, prompt := range prompts {
+	// 	result, _ := prompt.Run()
+	//
+	// 	results = append(results, result)
+	// }
 
-	for _, prompt := range prompts {
-		result, _ := prompt.Run()
+	// host, user, pass, port := results[0], results[1], results[2], results[3]
 
-		results = append(results, result)
-	}
+	// promptName := promptui.Prompt{
+	// 	AllowEdit: true,
+	// 	Default:   host,
+	// 	Label:     "name",
+	// 	Validate:  isNotBlank,
+	// }
 
-	host, user, pass, port := results[0], results[1], results[2], results[3]
-
-	promptName := promptui.Prompt{
-		AllowEdit: true,
-		Default:   host,
-		Label:     "name",
-		Validate:  isNotBlank,
-	}
-
-	name, _ := promptName.Run()
-
+	// name, _ := promptName.Run()
+	//
 	db.LocalDB.Create(&model.Database{
 		Driver: driver,
 		Host:   host,
