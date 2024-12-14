03e77b7838f14c622ab2755307ea1e0508873bdd
Lewis Steele <lewis.steele@posteo.net>
2024-12-14 10:58:49 +0000

    Run go fmt

--- internal/model/database.go
+++ internal/model/database.go
@@ -5,10 +5,10 @@ import "gorm.io/gorm"
 type Database struct {
 	gorm.Model
 	Database string
-	Driver string
-	Host   string
-	Name   string `gorm:"uniqueIndex"`
-	Pass   string
-	Port   string
-	User   string
+	Driver   string
+	Host     string
+	Name     string `gorm:"uniqueIndex"`
+	Pass     string
+	Port     string
+	User     string
 }
