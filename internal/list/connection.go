f6d8c88a3d9689cd6fb83ce7416e30d9efa5d8ab
Lewis Steele <lewis.steele@posteo.net>
2024-12-17 13:26:16 +0000

    Show host or path on database selection

--- internal/list/connection.go
+++ internal/list/connection.go
@@ -1,16 +1,36 @@
 package list
 
 import (
+	"fmt"
+
 	"github.com/charmbracelet/huh"
 	"github.com/lewissteele/dbat/internal/db"
+	"github.com/lewissteele/dbat/internal/model"
 )
 
 func RenderConnectionSelection() string {
 	var c string
 	options := []huh.Option[string]{}
 
-	for _, v := range db.UserDBNames() {
-		options = append(options, huh.NewOption[string](v, v))
+	var databases []model.Database
+	db.LocalDB.Find(&databases)
+
+	for _, d := range databases {
+		key := fmt.Sprintf(
+			"%s - %s@%s",
+			d.Name,
+			d.User,
+			d.Host,
+		)
+
+		if d.Driver == string(db.SQLite) {
+			key = fmt.Sprintf("%s - %s", d.Name, d.Path)
+		}
+
+		options = append(options, huh.NewOption[string](
+			key,
+			d.Name,
+		))
 	}
 
 	form := huh.NewForm(
