18e2e9f7b7d165021358072e7c5ef8c2ae31bc3b
Lewis Steele <lewis.steele@posteo.net>
2024-12-29 09:50:54 +0000

    Render table vertically

--- internal/table/table.go
+++ internal/table/table.go
@@ -3,47 +3,28 @@ package table
 import (
 	"fmt"
 	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/gosuri/uitable"
 )
 
 func Render(d []map[string]interface{}) {
-	var headers table.Row
+	t := uitable.New()
+	t.MaxColWidth = 80
+	t.Wrap = false
 
+	var headers table.Row
 	for k, _ := range d[0] {
 		headers = append(headers, k)
 	}
 
-	t := table.NewWriter()
-	t.AppendHeader(headers)
-
 	for _, row := range d {
-		var values []interface{}
-
 		for _, header := range headers {
 			k := header.(string)
-
-			if s, ok := row[k].(string); ok {
-				row[k] = ellipsis(s, 50)
-			}
-
-			values = append(values, row[k])
+			t.AddRow(k, row[k])
 		}
 
-		t.AppendRow(values)
+		t.AddRow("")
 	}
 
-	fmt.Println(t.Render())
+	fmt.Println(t)
 }
 
-func ellipsis(s string, maxLen int) string {
-	runes := []rune(s)
-
-	if len(runes) <= maxLen {
-		return s
-	}
-
-	if maxLen < 3 {
-		maxLen = 3
-	}
-
-	return string(runes[0:maxLen-3]) + "..."
-}
