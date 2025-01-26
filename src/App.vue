5fa6e23211cdce024a3df8d470dcc25c84925e71
Lewis Steele <lewis.steele@posteo.net>
2025-01-26 13:51:24 +0000

    Setup router for setup page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/core";
+import { RouterView } from "vue-router";
 
 const greetMsg = ref("");
 const name = ref("");
@@ -12,27 +13,8 @@ async function greet() {
 </script>
 
 <template>
-  <main class="container">
-    <h1>Welcome to Tauri + Vue</h1>
-
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <form class="row" @submit.prevent="greet">
-      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
-    </form>
-    <p>{{ greetMsg }}</p>
+  <main>
+    <RouterView></RouterView>
   </main>
 </template>
 
---
+++ src/router.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router";
+import MainView from "./views/MainView.vue";
+import SetupView from "./views/SetupView.vue";
+
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "main",
+      component: MainView,
+    },
+    {
+      path: "/setup",
+      name: "setup",
+      component: SetupView,
+    },
+  ],
+});
