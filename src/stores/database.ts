bba526b95596432cd104b7db6aa9ce78451944f6
Lewis Steele <lewis.steele@posteo.net>
2025-01-26 17:08:12 +0000

    Create database store

---
+++ src/stores/database.ts
@@ -0,0 +1,18 @@
+import { defineStore } from "pinia";
+import Connection from "../types/connection";
+
+export const useDatabaseStore = defineStore("database", {
+  state: () => ({
+    connections: [] as Connection[],
+  }),
+  getters: {
+    hasConnection(): boolean {
+      return this.connections.length > 0;
+    },
+  },
+  actions: {
+    save(connection: Connection) {
+      this.connections.push(connection);
+    },
+  },
+});
---
+++ src/types/connection.ts
@@ -0,0 +1,3 @@
+export default interface Connection {
+  host: string;
+}
