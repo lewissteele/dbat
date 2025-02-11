382d7762f62a8c063c380f3063dea72e030f71a6
Lewis Steele <lewis.steele@posteo.net>
2025-02-11 17:26:58 +0000

    Create connections list

--- src/views/ConnectionsView.vue
+++ src/views/ConnectionsView.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import ConnectionsList from '../components/ConnectionsList.vue';
+
 
 </script>
 
 <template>
 connections
+<ConnectionsList></ConnectionsList>
 </template>
