cfe81d1e19c2841ae4aa3cdb8e74f622fd68d9ca
Lewis Steele <lewis.steele@posteo.net>
2025-02-14 16:34:09 +0000

    Render stored connections

--- src/components/ConnectionsList.vue
+++ src/components/ConnectionsList.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
+import { useDatabaseStore } from '../stores';
+
+const db = useDatabaseStore();
 </script>
 
 <template>
+  <li v-for="conn in db.connections">
+    {{ conn.host }}
+    {{ conn.user }}
+    {{ conn.password }}
+    {{ conn.port }}
+  </li>
 </template>
 
 <style scoped>
