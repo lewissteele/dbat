959f29387f77a23df8835be6362b917260b0c553
Lewis Steele <lewis.steele@posteo.net>
2025-02-15 14:51:44 +0000

    Create sql editor component

---
+++ src/components/SqlEditor.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import CodeMirror from "vue-codemirror6";
+import { ref } from "vue";
+import { sql } from "@codemirror/lang-sql";
+
+const lang = sql();
+const code = ref("");
+</script>
+
+<template>
+  <code-mirror
+    v-model="code"
+    :lang="lang"
+    :dark="true"
+    basic
+    wrap
+    tab
+  />
+</template>
