371676ca5b006d58980570e28401607baa9ffa9a
Lewis Steele <lewis.steele@posteo.net>
2025-02-15 14:10:04 +0000

    Make list reactive

--- src/components/ConnectionsList.vue
+++ src/components/ConnectionsList.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { useDatabaseStore } from '../stores';
+import { storeToRefs } from 'pinia';
 
 const db = useDatabaseStore();
+const { connections } = storeToRefs(db);
 </script>
 
 <template>
-  <li v-for="conn in db.connections">
+  <li v-for="conn in connections">
     {{ conn.host }}
     {{ conn.user }}
     {{ conn.password }}
