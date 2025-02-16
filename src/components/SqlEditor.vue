618585f7cbb062659555001158bfab96886a256c
Lewis Steele <lewis.steele@posteo.net>
2025-02-16 14:54:13 +0000

    Set dialect to mysql

--- src/components/SqlEditor.vue
+++ src/components/SqlEditor.vue
@@ -1,19 +1,34 @@
 <script setup lang="ts">
 import CodeMirror from "vue-codemirror6";
 import { ref } from "vue";
-import { sql } from "@codemirror/lang-sql";
+import { sql, MySQL } from "@codemirror/lang-sql";
+import { NButton } from "naive-ui";
 
-const lang = sql();
+const lang = sql({
+  dialect: MySQL,
+});
 const code = ref("");
+
+function handle(): void {
+  console.log(code.value);
+}
 </script>
 
 <template>
   <code-mirror
     v-model="code"
-    :lang="lang"
-    :dark="true"
+    :lang=lang
+    :dark=true
+    :tab-size=2
     basic
     wrap
     tab
   />
+  <n-button @click="handle">Run</n-button>
 </template>
+
+<style>
+.cm-editor {
+  height: 500px;
+}
+</style>
