c2c985257dcd81e77ab7904107b8a6b6b12d1444
Lewis Steele <lewis.steele@posteo.net>
2025-02-16 14:46:22 +0000

    Remove unused menu options

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+use tauri::menu::MenuBuilder;
+use tauri::menu::SubmenuBuilder;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,6 +10,21 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let submenu = SubmenuBuilder::new(app, "App")
+                .quit()
+                .build()?;
+
+            let menu = MenuBuilder::new(app)
+                .items(&[
+                    &submenu,
+                ])
+                .build()?;
+
+            app.set_menu(menu)?;
+
+            Ok(())
+        })
         .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_pinia::init())
         .plugin(tauri_plugin_opener::init())
