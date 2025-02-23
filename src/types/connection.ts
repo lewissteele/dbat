a899a2973f43896bc11155ce394c42cb68fe10d8
Lewis Steele <lewis.steele@posteo.net>
2025-02-23 16:28:01 +0000

    Add driver

--- src/types/connection.ts
+++ src/types/connection.ts
@@ -1,7 +1,10 @@
+import { Driver } from "./driver";
+
 export interface Connection {
+  database: string;
+  driver: Driver;
   host: string;
   password: string;
   port: string;
   user: string;
-  uuid: string;
 }
