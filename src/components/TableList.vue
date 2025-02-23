539610cf9ca27146023e051e5ef5909cc0a965a4
Lewis Steele <lewis.steele@posteo.net>
2025-02-23 17:27:40 +0000

    Render table list

---
+++ src/components/TableList.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useDatabaseStore } from '../stores';
+import { NList, NListItem } from 'naive-ui';
+
+const db = useDatabaseStore();
+
+const { tables } = storeToRefs(db);
+
+</script>
+
+<template>
+  <n-list>
+    <n-list-item v-for="table in tables">
+      {{ table.name }}
+    </n-list-item>
+  </n-list>
+</template>
--- src/stores/database.ts
+++ src/stores/database.ts
@@ -1,10 +1,11 @@
 import Database from "@tauri-apps/plugin-sql";
-import { Connection, Driver } from "../types";
+import { Connection, Driver, Table } from "../types";
 import { defineStore } from "pinia";
 
 export const useDatabaseStore = defineStore("database", {
   state: () => ({
     connection: null as Database | null,
+    tables: [] as Table[],
   }),
   actions: {
     async connect(connection: Connection): Promise<void> {
@@ -18,6 +19,19 @@ export const useDatabaseStore = defineStore("database", {
       }
 
       this.connection = await Database.load(path);
+
+      this.refresh();
     },
+    async refresh(): Promise<void> {
+      if (!this.connection) {
+        return;
+      }
+
+      const tables = await this.connection.select("show tables") as any[];
+
+      this.tables = tables.map((table) => ({
+        name: Object.values(table)[0] as string,
+      }));
+    }
   },
 });
