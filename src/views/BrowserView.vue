03fe303b0f74b847fa85ac1222d62af1a238d36f
Lewis Steele <lewis.steele@posteo.net>
2025-04-04 12:10:15 +0100

    Render list of db tables and allow selection

--- src/views/BrowserView.vue
+++ src/views/BrowserView.vue
@@ -1,6 +1,31 @@
 <script setup lang="ts">
+import { Table } from "../types";
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
+import { useDatabaseStore } from "../stores";
+
+const db = useDatabaseStore();
+const { tables } = storeToRefs(db);
+const selectedTable = ref("");
+
+function selectTable(table: Table): void {
+  selectedTable.value = table.name;
+}
 </script>
 
 <template>
-  <button type="button" class="btn btn-primary">Test</button>
+  <ul class="list-group list-group-flush">
+    <a
+      :class='{ "list-group-item-light": table.name == selectedTable }'
+      @click="selectTable(table)"
+      class="list-group-item list-group-item-action"
+      href="#"
+      v-for="table in tables"
+    >
+      {{ table.name }}
+    </a>
+  </ul>
 </template>
+
+<style lang="scss" scoped>
+</style>
