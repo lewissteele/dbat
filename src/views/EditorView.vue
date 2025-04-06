149999747f822470dfbe8fcb608da972aab3efd0
Lewis Steele <lewis.steele@posteo.net>
2025-04-06 15:10:51 +0100

    Add browser button

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 import SqlEditor from "../components/SqlEditor.vue";
+import router from "../router";
 </script>
 
 <template>
+  <button
+    @click='() => router.replace({ name: "browser" })'
+    class="btn btn-primary col"
+  >
+    Browser
+  </button>
   <SqlEditor />
 </template>
