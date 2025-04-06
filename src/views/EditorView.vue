3ac321b3ff15de4bb89ba3181cbf0fe8b7cce83f
Lewis Steele <lewis.steele@posteo.net>
2025-04-06 15:29:10 +0100

    Use bootstrap instead of naive

--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts">
-import SqlEditor from "../components/SqlEditor.vue";
+import CodeMirror from "vue-codemirror6";
+import Table from "../components/Table.vue";
 import router from "../router";
+import { MySQL, sql } from "@codemirror/lang-sql";
+import { ref } from "vue";
+import { useDatabaseStore } from "../stores";
+
+const code = ref("select * from migrations");
+const tableData = ref([] as any[]);
+
+const lang = sql({
+  dialect: MySQL,
+});
+
+const db = useDatabaseStore();
+
+async function handle(): Promise<void> {
+  const result = await db.connection?.select(code.value) as any[];
+  tableData.value = result;
+}
 </script>
 
 <template>
@@ -10,5 +28,23 @@ import router from "../router";
   >
     Browser
   </button>
-  <SqlEditor />
+
+  <code-mirror
+    :dark="true"
+    :lang="lang"
+    :tab-size="2"
+    basic
+    tab
+    v-model="code"
+    wrap
+  />
+
+  <button
+    @click="handle"
+    class="btn btn-primary"
+  >
+    Run
+  </button>
+
+  <Table :data="tableData"></Table>
 </template>
