f35d2b5c5b35275d2fceadc84c0bfd14a22c02a6
Lewis Steele <lewis.steele@posteo.net>
2025-04-09 15:09:37 +0100

    Use bootstrap to render connections list

--- src/views/ConnectionsView.vue
+++ src/views/ConnectionsView.vue
@@ -1,8 +1,35 @@
 <script setup lang="ts">
-import ConnectionsList from "../components/ConnectionsList.vue";
+import router from "../router";
+import { Connection } from "../types";
+import { storeToRefs } from "pinia";
+import { useConfigStore, useDatabaseStore } from "../stores";
+
+const config = useConfigStore();
+const db = useDatabaseStore();
+
+const { connections } = storeToRefs(config);
+
+async function selectConnection(connection: Connection): Promise<void> {
+  config.activeConnection = connection;
+  await db.connect(config.activeConnection);
+  router.replace({ name: "browser" });
+}
 </script>
 
 <template>
-  connections
-  <ConnectionsList></ConnectionsList>
+  <div class="container pt-5">
+    <ul class="list-group col-12">
+      <a
+        @click="async () => await selectConnection(connection)"
+        class="list-group-item list-group-item-action"
+        href="#"
+        v-for="connection in connections"
+      >
+        {{ connection.user }}@{{ connection.host }}
+      </a>
+    </ul>
+  </div>
 </template>
+
+<style scoped>
+</style>
